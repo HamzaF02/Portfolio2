bc8191ea81ce53903ff4f2f6011da26296e0c167
AdnanB1203 <s364519@oslomet.no>
2023-05-01 16:39:32 +0200

    Added flags

--- src/application.py
+++ src/application.py
@@ -9,21 +9,33 @@ parser.add_argument('-s', '--server', action='store_true',
                     help='Runs program in server mode')
 parser.add_argument('-c', '--client', action='store_true',
                     help='Runs program in client mode')
+parser.add_argument('-i', '--ip', type=str, default='127.0.0.1',
+                    help='Selects the ip address of the server')
+parser.add_argument('-p', '--port', type=int, default=8088,
+                    help='Allows you to select the port')
+parser.add_argument('-f', '--filename', type=str,
+                    help='The file to transfer')
+parser.add_argument('-r', '--reliablemethod', type=str,
+                    help='Starts the reliable method chosen')
+parser.add_argument('-t', '--testcase', type=str,
+                    help='Starts the test case chosen')
+
 
 args = parser.parse_args()
 
 
 def client():
     clientSocket = socket(AF_INET, SOCK_DGRAM)
-    port = 8088
-    ip = "127.0.0.1"
+    port = args.port
+    ip = args.ip
+
     try:
         clientSocket.connect((ip, port))
     except:
         print("Failed to connect")
         sys.exit()
 
-    name = input("Filename: ")
+    name = args.filename
     clientSocket.send(name.encode())
 
     f = open(name, "rb")
@@ -40,8 +52,8 @@ def client():
 
 def server():
     serverSocket = socket(AF_INET, SOCK_DGRAM)
-    port = 8088
-    ip = "127.0.0.1"
+    port = args.port
+    ip = args.ip
 
     try:
         serverSocket.bind((ip, port))
